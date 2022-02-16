b49e597321361af69052d089d41d82124ed7b816
Nuhel <nuheljft@gmail.com>
2022-02-16 23:13:18 +0600

    Order Services migration created.

---
+++ database/migrations/2022_02_16_170930_create_order_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_services', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id');
+            $table->bigInteger('service_id');
+            $table->bigInteger('employee_id');
+            $table->double('quantity');
+            $table->double('price');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_services');
+    }
+}
